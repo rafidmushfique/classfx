f037ff864a13cd43a53f4e4d4f5008b744d0f61c
Md Rafid Mushfique <49343788+RafidMushfique@users.noreply.github.com>
2019-07-22 10:26:42 +0600

    commit

--- src/classfx/SignupController.java
+++ src/classfx/SignupController.java
@@ -5,11 +5,18 @@
  */
 package classfx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,10 +25,13 @@ import javafx.scene.control.Button;
  */
 public class SignupController implements Initializable {
 
+<<<<<<< HEAD
+=======
     @FXML
     private Button signup;
     @FXML
-    private Button signup1;
+    private Button back;
+>>>>>>> e7d6f3b7516b69424c5628e605387489020d655b
 
 
 
@@ -39,4 +49,19 @@ public class SignupController implements Initializable {
         // TODO
     }    
     
+    public void backButton(ActionEvent event) 
+  {
+      try
+      {
+      Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+      Scene nextScene = new Scene(root);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      window.setScene(nextScene);
+      window.show();
+      }
+      catch(Exception e)
+      {
+      e.printStackTrace();
+      }
+  }
 }
