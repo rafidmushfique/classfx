de884b29b10beb715d5596cb379e13c61396e221
Md Rafid Mushfique <49343788+RafidMushfique@users.noreply.github.com>
2019-07-23 12:36:36 +0600

    master commit

--- src/classfx/AdminController.java
+++ src/classfx/AdminController.java
@@ -5,9 +5,17 @@
  */
 package classfx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -18,6 +26,71 @@ import javafx.stage.Stage;
  */
 public class AdminController implements Initializable {
 
+    @FXML
+    private Button studentinfo;
+    @FXML
+    private Button facultyinfo;
+    @FXML
+    private Button accountentinfo;
+    @FXML
+    private Button approvelist;
+    @FXML
+    private Button classschedule;
+
+      public void studentinfo(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("StuInfo.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
+          public void facultyinfo(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("FacultyInfo.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
+              public void accountentinfo(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("AccInfo.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
+                  public void approvelist(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("Approve.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
+                      public void classschedule(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("ClassSchedule.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
+                          public void Backbutton(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/classfx/CreatingConnection.java
+++ src/classfx/CreatingConnection.java
@@ -17,7 +17,7 @@ public class CreatingConnection {
     public static Connection con;
     public CreatingConnection() throws SQLException
     {
-         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classch?zeroDateTimeBehavior=convertToNull", "root", "root");
+         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classch?zeroDateTimeBehavior=convertToNull", "root", "");
          // this for mhr75 connections with DB
     }
 }
