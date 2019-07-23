2542040cbdc620824389bec442de0d689e8b3586
Md Rafid Mushfique <49343788+RafidMushfique@users.noreply.github.com>
2019-07-24 01:57:42 +0600

    minor commit

--- src/classfx/AccountentController.java
+++ src/classfx/AccountentController.java
@@ -5,6 +5,7 @@
  */
 package classfx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,7 +17,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -171,6 +176,15 @@ public class AccountentController implements Initializable {
         
     
     }
+     public void backbutton(ActionEvent event) throws IOException
+  {
+       Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+       Scene nextScene = new Scene(root);
+             Stage window= (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+             window.setScene(nextScene);
+             window.show();
+      
+  }
     
     
     
